75ef9c080bf3a7c07a531755b5af64f05b9309fa
umeshbalajis <umeshbalaji987@gmail.com>
2023-08-04 11:42:47 +0530

    updating files

--- iam-count/main.tf
+++ iam-count/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIARXM45NH26CHNOWEE"
-  secret_key = "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"
+  access_key = "AKIARXdvdfM45NH2TTUOEYMT"
+  secret_key = "jFxncG4qbQfdbdfV4vNOpOYFNRycuph1MFUtQJ/M1lE1z"
 }
 
 #This block will create individual user as mentioned in the variables
@@ -9,6 +9,7 @@ resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
 }
+
 #this block will create user like neo.1, neo.2, neo.3
 /*resource "aws_iam_user" "example" {
   count = 3
@@ -16,12 +17,12 @@ resource "aws_iam_user" "example" {
 }*/
 
 #Print first array user i.e 1st user Arn alone
-output "first_arn" {
+/*output "first_arn" {
   value       = aws_iam_user.example[0].arn
   description = "The ARN for the first user"
-}
+}*/
 #Print all user ARN
-output "all_arns" {
+/*output "all_arns" {
   value       = aws_iam_user.example[*].arn
   description = "The ARNs for all users"
-}
+}*/
