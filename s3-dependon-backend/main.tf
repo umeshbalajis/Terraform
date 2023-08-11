14a118520166eae18caf156dc38d6d5f42cc6dd5
umesh <64860725+umeshbalajis@users.noreply.github.com>
2023-08-11 12:55:15 +0530

    Update main.tf

--- s3-dependon-backend/main.tf
+++ s3-dependon-backend/main.tf
@@ -15,8 +15,15 @@ resource "aws_s3_bucket" "example_bucket" {
   bucket = "testqtreedata4456"     //update your unique bucket name          
   tags = {
     Name = "MyExampleBucket"
+  }example_bucket
+}
+resource "aws_s3_bucket_versioning" "example_bucket" {
+  bucket = aws_s3_bucket.example_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
   }
 }
+
 resource "aws_s3_bucket_lifecycle_configuration" "example_bucket_lifecycle" {
   rule {
     id      = "example_rule"
